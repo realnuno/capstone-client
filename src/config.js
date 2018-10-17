e057d0eea4bb9b34ed6dee191518d61bcab78644
realnuno <inbeom918@gmail.com>
2018-10-17 19:03:46 -0400

    change server url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://fierce-sea-27586.herokuapp.com';
+    process.env.REACT_APP_API_BASE_URL || 'https://travelers-server.herokuapp.com/';
 export const GOOGLE_MAP_API = 'AIzaSyDE0B5I7RUPEjusRik4arRAAa8LQnA2c0s';
 
 
